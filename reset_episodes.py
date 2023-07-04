01589d7c6cf4a6f5e1d36960a26e2a4c6c21b89e
MortexAG <ahmedgouda100@gmail.com>
2023-07-04 14:24:51 +0300

    Update reset_episodes.py

--- reset_episodes.py
+++ reset_episodes.py
@@ -6,5 +6,5 @@ os.remove("episodes.db")
 os.remove("stored_spells.db")
 
 for databases in glob.glob("./databases/*"):
-    shutil.copy("./databases/episodes.db", "../")
-    shutil.copy("./databases/stored_spells.db", "../")
+    shutil.copy("./databases/episodes.db", "./")
+    shutil.copy("./databases/stored_spells.db", "./")
